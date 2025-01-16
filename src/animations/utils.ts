7f1584cd934c0ef2183f1a3857b287de5350f206
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-17 01:42:22 +0530

    Add validateProgress function to ensure progress value is between 0 and 1

--- src/animations/utils.ts
+++ src/animations/utils.ts
@@ -1,3 +1,7 @@
+
+/**
+ * Validate the progress value
+ */
 export const validateProgress = (progress: number) => {
   if (progress < 0 || progress > 1) {
     throw new Error("Progress must be between 0 and 1");
