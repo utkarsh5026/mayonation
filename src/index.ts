6e5486b7e4d3bbac31ebcf6c6601e0989153c3fc
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-01-18 02:55:42 +0530

    Import PathBuilder from the SVG path-builder module in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import PathBuilder from "./animations/svg/path-builder";
+
 export * from "./core/mayo";
 export * from "./core/ease_fns";
 export { PropertiesConfig } from "./core/config";
+export { PathBuilder };
