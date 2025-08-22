35d7fb78b04b55709832440cebe84d7e36f8de09
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-22 14:20:25 +0530

    Add new prop-names.ts file with sets of CSS properties for animations
    - Introduce COLOR_PROPERTIES, MULTI_VALUE_PROPERTIES, and EXTENDED_ANIMATABLE_PROPERTIES to define animatable CSS properties.
    - This addition enhances the animation framework by providing a structured way to manage and reference CSS properties used in animations.

---
+++ src/animations/styles/prop-names.ts
@@ -0,0 +1,68 @@
+export const COLOR_PROPERTIES = new Set<string>([
+  "backgroundColor",
+  "color",
+  "borderColor",
+  "outlineColor",
+  "textDecorationColor",
+  "textEmphasisColor",
+  "caretColor",
+  "columnRuleColor",
+  "textShadow",
+  "boxShadow",
+]);
+
+export const MULTI_VALUE_PROPERTIES = new Set<string>([
+  "boxShadow",
+  "textShadow",
+  "filter",
+  "backdropFilter",
+]);
+
+// Extended set of animatable CSS properties
+export const EXTENDED_ANIMATABLE_PROPERTIES = new Set<string>([
+  // Layout
+  "width",
+  "height",
+  "top",
+  "left",
+  "right",
+  "bottom",
+  "margin",
+  "marginTop",
+  "marginRight",
+  "marginBottom",
+  "marginLeft",
+  "padding",
+  "paddingTop",
+  "paddingRight",
+  "paddingBottom",
+  "paddingLeft",
+
+  // Visual
+  "opacity",
+  "backgroundColor",
+  "color",
+  "borderColor",
+  "borderWidth",
+  "borderRadius",
+  "boxShadow",
+  "textShadow",
+
+  // Typography
+  "fontSize",
+  "lineHeight",
+  "letterSpacing",
+  "wordSpacing",
+
+  // Filters
+  "filter",
+  "backdropFilter",
+
+  // Flex & Grid
+  "flexGrow",
+  "flexShrink",
+  "flexBasis",
+  "gridTemplateColumns",
+  "gridTemplateRows",
+  "gap",
+]);
