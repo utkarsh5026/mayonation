3e03d3a148640fa66405f59493c7b961c1a9e3db
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-22 12:02:40 +0530

    Refactor animation API by introducing timeline and mayo functions
    - Add timeline function to create animation timelines with optional looping.
    - Introduce mayo function for initializing Mayonation instances with flexible configuration options.
    - Remove the deprecated mayo.ts file to streamline the codebase.
    - Update index.ts to include new exports for timeline and mayo functions, enhancing the animation framework's usability.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,23 @@ import PathBuilder from "./svg/path/path-builder";
 export * from "./core/ease-fns";
 export { PropertiesConfig } from "./core/config";
 export { PathBuilder };
-export { Mayonation, MayonationConfig } from "@/api";
+import { Mayonation, MayonationConfig } from "@/api";
+import { Timeline } from "./timeline";
+import { ElementLike } from "./utils/dom";
+
+export function timeline(options?: { loop?: boolean }): Timeline {
+  return new Timeline(options);
+}
+
+export function mayo(config: MayonationConfig): Mayonation;
+export function mayo(
+  target: ElementLike,
+  config: Omit<MayonationConfig, "target">
+): Mayonation;
+
+export function mayo(targetOrConfig: any, config?: any): Mayonation {
+  if (config) {
+    return new Mayonation({ target: targetOrConfig, ...config });
+  }
+  return new Mayonation(targetOrConfig);
+}
