5a50ae886ab3f7ec580147613c8526fb01a2f924
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-22 12:05:36 +0530

    Remove keyframe management classes and associated types

--- src/keyframe/index.ts
+++
@@ -1,4 +0,0 @@
-import { BaseKeyframeManager } from "./keyframe";
-import type { BaseKeyframe, ProcessedBaseKeyframe } from "./types";
-
-export { BaseKeyframe, ProcessedBaseKeyframe, BaseKeyframeManager };
