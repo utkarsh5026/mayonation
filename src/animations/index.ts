3561acf358150d51af964df7e041e38b58ebf931
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-25 19:23:31 +0530

    Refactor index.ts: remove commented sections for cleaner code and maintain export structure.

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -1,10 +1,6 @@
-// Animation handlers
 export { StyleAnimator } from "./styles";
-export { TransformHandler } from "./transform/handler";  
+export { TransformHandler } from "./transform/handler";
 export { PropertyManager } from "./property-manager";
 
-// Transform utilities (often used separately)
 export * as transform from "./transform";
-
-// Style utilities  
 export * as styles from "./styles";
