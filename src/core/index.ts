31c6c29f9bef10005955128f29fbfa26959fa053
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-08-27 22:12:34 +0530

    Refactor index.ts: streamline export statements for AnimationValue and NumericValue, improving code clarity and organization.

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,14 +1,13 @@
-// Core animation system
 export { AnimationEngine } from "./engine";
 export * from "./ease-fns";
-export { 
-  AnimationValue, 
-  NumericValue, 
+export {
+  AnimationValue,
+  NumericValue,
   ColorValue,
   type ColorSpace,
   isNumericValue,
   isColorValue,
   isRGBColor,
   isHSLColor,
-  createValue
+  createValue,
 } from "./animation-val";
